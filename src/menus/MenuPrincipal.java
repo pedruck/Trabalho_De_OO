d6a44775c3f056656d22b400cca99025b6a324a4
pedruck <pedrodmreis@gmail.com>
2025-01-28 19:26:39 -0300

    organizacao das classes de menu

--- src/menus/MenuPrincipal.java
+++ src/menus/MenuPrincipal.java
@@ -1,38 +1,54 @@
 package menus;
 
+import javax.swing.*;
+
 public class MenuPrincipal
 {
-    public static String MenuPrincipalGeral()
+    public static int MenuPrincipalGeral()
     {
         String opcoes =
                 "1 - Cadastros\n" +
                 "2 - Consultas\n" +
                 "0 - Sair";
 
+        String option_pannel = JOptionPane.showInputDialog(opcoes);
 
-        return opcoes;
+        int opcao = Integer.parseInt(option_pannel);
+
+
+
+        return opcao;
 
     }
 
-    public static String MenuPrincipalCadastros()
+    public static int MenuPrincipalCadastros()
     {
         String opcoes =
                         "1 - Cadastrar Médico\n" +
                         "2 - Cadastrar Paciente\n" +
                         "0 - Voltar";
 
-        return opcoes;
+
+        String option_pannel = JOptionPane.showInputDialog(opcoes);
+        int opcao = Integer.parseInt(option_pannel);
+
+        return opcao;
 
     }
 
-    public static String MenuPrincipalConsultas()
+    public static int MenuPrincipalConsultas()
     {
         String opcoes =
                         "1 - Agendar Consulta\n" +
                         "2 - Pagamento de Consultas\n" +
                         "0 - Voltar";
 
-        return opcoes;
+        String option_pannel = JOptionPane.showInputDialog(opcoes);
+        int opcao = Integer.parseInt(option_pannel);
+
+        return opcao;
+
+
 
     }
 
